8f585081619f808e32972aeab01b33a5d35886bc
GigiYaz <74292877+GigiYaz@users.noreply.github.com>
2021-01-14 11:17:44 +0100

    Add files via upload

---
+++ ProPro/js/profile.js
@@ -0,0 +1,8 @@
+
+
+// this is what is called after pushing the button
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello, I´m John Doe";
+}
+
+// this above is what is what the text will change to
